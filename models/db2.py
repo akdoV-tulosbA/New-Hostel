2ad0d6d284a0f9bf30ec5bbd5fd100761a59331d
Aayush Naik <aayushnaik17@gmail.com>
2015-04-17 20:05:51 +0530

    added fields to default registration form

--- models/db2.py
+++ models/db2.py
@@ -1,17 +1,4 @@
-from gluon.tools import Auth
-auth = Auth(db)
-auth.define_tables(username=False,signature=False)
-
-auth.settings.extra_fields['auth_user']= [
-  Field('address'),
-  Field('city'),
-  Field('zip'),
-  Field('phone')]
-
 db.define_table('newspaper',
-                Field('choice_of_newspaper', requires=IS_IN_SET(['The Times of India', 'The Hindu'])),
+                Field('choice_of_newspaper', requires=IS_IN_SET(['The Times of India', 'The Hindu', 'The White Fur'])),
                 Field('subscription_type', requires=IS_IN_SET(['Yearly', 'Monthly'])),
                 Field('subscriber', 'reference auth_user', writable=False, readable=False))
-
-
-auth.define_tables(username=True)
